84a863ae7c73e5c4bbbfa7893de97908fa48ff59
lee1682 <lee@cubiq.co.uk>
2021-12-09 20:06:52 +0000

    /u

--- frontpage.php
+++ frontpage.php
@@ -1,5 +1,6 @@
 <?php /* Template Name: Front Page */?>
 <?php get_header()?>
+
 <section class="hero row">
     <div class="hero_text col-sm-12 col-md-6 ">
         <h1>Blue Panda Agency</h1>
@@ -9,4 +10,23 @@
     <div class="hero_img col-sm-12 col-md-6 ">
         <img src="" alt="">
     </div>
+</section>
+
+<section class="text-image row">
+    <div class="col-sm-12 col-md-6 text-image_image">
+        <img src="" alt="">
+    </div>
+    <div class="col-sm-12 col-md-6 text-image_text">
+        <p class="tagline">Web Development & Digital Marketing</p>
+        <h2> Grow Your Online Business</h2>
+        <p>Cubiq is a top UK digital agency established in 2004. Our team of search engine optimisation
+            professionals successfully help a range of clients to achieve their online goals. We improve performance,
+            content quality
+            and user experience to increase organic traffic and generate quality leads for your business. We help to
+            increase rankings nationally for mid to
+            large size clients and can also help smaller businesses with local SEO to target specific locations and
+            appear on Google
+            Maps.</p>
+    </div>
+
 </section>
