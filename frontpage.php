3fa672b28b39a79b260b91ed4bc007fa001fc31f
lee1682 <lee@cubiq.co.uk>
2021-12-13 18:58:15 +0000

    three-col-text

--- frontpage.php
+++ frontpage.php
@@ -28,5 +28,39 @@
             appear on Google
             Maps.</p>
     </div>
+</section>
+
+<section class="three-col-text row">
+    <div class="col-sm-12">
+        <h2>Services
+        </h2 class="seperator">
+    </div>
+    <div class="row">
+        <div class="col-sm-12 col-md-4">
+            <h3>Service Item</h3>
+            <p>and user experience to increase organic traffic and generate quality leads for your business. We help to
+                increase rankings nationally for mid to
+                large size clients and can also help smaller businesses with local SEO to target specific locations and
+                appear on Google
+                Maps</p>
+        </div>
+        <div class="col-sm-12 col-md-4">
+            <h3>Service Item</h3>
+            <p>and user experience to increase organic traffic and generate quality leads for your business. We help to
+                increase rankings nationally for mid to
+                large size clients and can also help smaller businesses with local SEO to target specific locations and
+                appear on Google
+                Maps</p>
+        </div>
+        <div class="col-sm-12 col-md-4">
+            <h3>Service Item</h3>
+            <p>and user experience to increase organic traffic and generate quality leads for your business. We help to
+                increase rankings nationally for mid to
+                large size clients and can also help smaller businesses with local SEO to target specific locations and
+                appear on Google
+                Maps</p>
+        </div>
+    </div>
+
 
 </section>
