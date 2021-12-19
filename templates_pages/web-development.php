149d7a0531f719143e19786063775b14f3aedd24
lee1682 <lee@cubiq.co.uk>
2021-12-19 17:35:37 +0000

    product thumnbs

--- templates_pages/web-development.php
+++ templates_pages/web-development.php
@@ -3,3 +3,4 @@
 <?php get_template_part("templates_sections/hero"); ?>
 <?php get_template_part("templates_sections/two-col-text-image"); ?>
 <?php get_template_part("templates_sections/two-col-text"); ?>
+<?php get_template_part("templates_sections/product-thumbs"); ?>
